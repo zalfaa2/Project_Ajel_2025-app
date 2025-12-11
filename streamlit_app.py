054a03f42db9f5bef051eb7c5854721de708da6f
zalfaa2 <zalfahanifah770@gmail.com>
2025-12-11 15:25:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 ajel project lpk 2025")
+st.write(import streamlit as st
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:"))
